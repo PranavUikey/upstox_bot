558e5e01842c754e3f6196366a7360b6762b088f
pranav <pranavuiih@gmail.con>
2025-06-06 17:30:51 +0530

    added steps till EC2 instance in README.md

--- config.py
+++ config.py
@@ -10,13 +10,3 @@ class AWSConfig:
         return self.ssm_client.get_parameter(Name=name, WithDecryption=with_decryption)
 
 
-# aws_config = AWSConfig()
-# access_token = aws_config.get_parameter('/upstox/access_token')['Parameter']['Value']
-# client_id = aws_config.get_parameter('/upstox/client_id')['Parameter']['Value']
-# redirect_uri = aws_config.get_parameter('/upstox/redirect_uri')['Parameter']['Value']
-# client_secret = aws_config.get_parameter('/upstox/client_secret')['Parameter']['Value']
-
-# print(f"Access Token: {access_token}")
-# print(f"Client ID: {client_id}")
-# print(f"Redirect URI: {redirect_uri}")
-# print(f"Client Secret: {client_secret}")
